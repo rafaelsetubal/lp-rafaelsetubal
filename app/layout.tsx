b0a0299d8ce2213b2a14bd87769b584345b32270
Rafael <marzcreativedesign@gmail.com>
2026-09-18 21:37:43 -0300

    chore: add microsoft clarity tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Fustat, Inter_Tight } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 
 const fustat = Fustat({
@@ -37,9 +38,17 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body
-
         className={`${fustat.variable} ${interTight.variable} font-sans antialiased text-[#111111] bg-white selection:bg-[#3D6AFF] selection:text-white`}
       >
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ykhpfcjz0q");
+          `}
+        </Script>
         {children}
       </body>
     </html>
